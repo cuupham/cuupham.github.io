3755fc3ea177e3bc2a6a69f7ee8ac0e33f335481
cuupham <cuu9991@gmail.com>
2024-03-31 15:53:57 +0700

    update setVolume func

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
-function setVolume(videoId, volumeLevel) {
-    let video = document.getElementById(videoId);
-    if (video) {
-        video.volume = volumeLevel;
-    }
+function setVolume(className, volume) {
+    const video_list = document.querySelectorAll('.' + className);
+
+    video_list.forEach(video_list => {
+        const videos = video_list.querySelectorAll('video');
+
+        videos.forEach(singleVideo => {
+            singleVideo.volume = volume;
+        });
+    });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    setVolume("video-container", 0.86);
+});
