413ce459e9ed4f813cd48c50f23f958d496a0196
cuupham <cuu9991@gmail.com>
2024-04-04 11:54:08 +0700

    refactor script.js

--- scripts.js
+++ scripts.js
@@ -1,42 +1,40 @@
 
 /* -- Xử lý Volume và Pause Video -- */
 document.addEventListener('DOMContentLoaded', function () {
-    setVolume("low-volume", 0.9);
-    setVolume("loud-volume", 0.5);
-
     const videos = document.querySelectorAll('video');
+
+    setVolume(videos, "low-volume", 0.9);
+    setVolume(videos, "loud-volume", 0.5);
+
+
     videos.forEach(video => {
         video.addEventListener('play', function () {
-            pauseOtherVideos(video);
+            pauseOtherVideos(video, videos);
         });
-
-
     });
 });
 
-function setVolume(className, volume) {
-    const videos = document.querySelectorAll('.' + className);
-
+function setVolume(videos, className, volume) {
     videos.forEach(video => {
-        video.volume = volume;
+        if (video.classList.contains(className)) {
+            video.volume = volume;
+        }
     });
 }
 
-function pauseOtherVideos(currentVideo) {
-    const videos = document.querySelectorAll('video');
-
-    videos.forEach(video => {
+function pauseOtherVideos(currentVideo, allVideos) {
+    allVideos.forEach(video => {
         if (video !== currentVideo && !video.paused) {
             video.pause();
         }
     });
 }
 
-
+/* -- Xử lý Youtube --  */
 
 
 /* -- Xử lý chức năng download -- */
-async function checkTikTokUrl(url) {
+async function verifyTikTokUrl(url) {
     try {
         const response = await fetch(`https://www.tiktok.com/oembed?url=${encodeURIComponent(url)}`, {
             method: 'GET',
@@ -62,88 +60,3 @@ async function checkTikTokUrl(url) {
         return false;
     }
 }
-
-function checkYoutubeUrl(url) {
-
-}
-
-function downloadYoutubeVideo() {
-    let url_youtube = document.getElementById("url_youtube").value
-
-    // checkYoutubeUrl(url_youtube)
-    //     .then(result => {
-    //         if (result) {
-    //             //alert('ok')
-    //             downloadVideo(url_youtube,"youtube")
-    //         } else {
-    //             //alert('URL không hợp lệ')
-    //         }
-    //     })
-    //     .catch(error => {
-    //         console.error('Xảy ra lỗi khi kiểm tra URL:', error);
-    //         alert('Xảy ra lỗi khi kiểm tra URL:', error)
-    //     });
-
-    alert("Tinh nang dang phat trien")
-}
-
-function downloadTiktokVideo() {
-    let url_tiktok = document.getElementById("url_tiktok").value
-
-    checkTikTokUrl(url_tiktok)
-        .then(result => {
-            if (result) {
-                //alert('ok')
-                downloadVideo(url_tiktok, "tiktok")
-            } else {
-                alert('URL không hợp lệ')
-            }
-        })
-        .catch(error => {
-            console.error('Xảy ra lỗi khi kiểm tra URL:', error);
-            alert('Xảy ra lỗi khi kiểm tra URL:', error)
-        });
-}
-
-
-
-function downloadVideo(videoUrl, platform) {
-    // Kiểm tra và xử lý tải video từ URL dựa trên nền tảng (Youtube hoặc Tiktok)
-    if (platform === "youtube") {
-        // Xử lý tải video từ Youtube
-        alert("Downloading video from Youtube: " + videoUrl);
-        // Chạy hàm với URL cần tải xuống
-        processYoutube(videoUrl)
-
-    } else if (platform === "tiktok") {
-        // Xử lý tải video từ Tiktok
-        alert("Downloading video from Tiktok: " + videoUrl);
-        // Chạy hàm với URL cần tải xuống
-        processTikTok(videoUrl)
-    } else {
-        // Nền tảng không hợp lệ
-        alert("Invalid platform!");
-    }
-}
-
-function processTikTok(url) {
-    // Step 1: Mở newtab trong chế độ ẩn danh và truy cập URL https://snaptik.app/
-    const newTab = window.open('https://snaptik.app/', '_blank', 'noopener,noreferrer');
-
-    // Step 2: Chờ cho trang snaptik.app load hoàn tất
-    newTab.onload = function () {
-        // Step 3: Lấy đối tượng input trên trang snaptik.app
-        const inputElement = newTab.document.getElementById('url');
-        if (inputElement) {
-            // Step 4: Lấy giá trị URL từ input trên page download.html của bạn
-            const urlToPaste = document.getElementById('url_tiktok').value;
-            console.log(url)
-            // Step 5: Gán giá trị URL vào input trên trang snaptik.app
-            inputElement.value = urlToPaste;
-        } else {
-            console.error('Input element not found on the page.');
-        }
-    };
-}
-
-
