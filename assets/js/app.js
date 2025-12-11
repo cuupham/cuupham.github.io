a4296ed4729e340084cd895ed8cb9787e2ee40e4
cuupham <cuu9991@gmail.com>
2025-12-11 12:56:25 +0700

    update: add cache in load json

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
+const cache = {};
+
 async function loadCategory(category) {
   const content = document.getElementById("content");
   content.innerHTML = `<p>Loading...</p>`;
 
   try {
-    const res = await fetch(`assets/data/${category}.json`);
-    const data = await res.json();
+    let data;
+    if (cache[category]) {
+      data = cache[category]; // lấy từ cache
+    } else {
+      const res = await fetch(`assets/data/${category}.json`);
+      data = await res.json();
+      cache[category] = data; // lưu vào cache
+    }
 
     content.innerHTML = `<h2>${category.toUpperCase()}</h2>`;
-
     data.forEach(item => {
       const block = document.createElement("div");
       block.className = "snippet";
-
       block.innerHTML = `
         <h3>${item.title}</h3>
         <pre><code>${item.content}</code></pre>
       `;
-
       content.appendChild(block);
     });
   } catch (error) {
